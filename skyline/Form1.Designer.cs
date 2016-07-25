16bd67fb16b24315da95a0d4d3b5055f92106b42
向敬光 <1991887681@qq.com>
2016-07-25 17:09:36 +0800

    Signed-off-by: 向敬光 <1991887681@qq.com>

---
+++ skyline/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace skyline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.axTE3DWindow1 = new AxTerraExplorerX.AxTE3DWindow();
+            this.axTEInformationWindow1 = new AxTerraExplorerX.AxTEInformationWindow();
+            this.buttonGetTree = new System.Windows.Forms.Button();
+            this.buttonSaveToXml = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.shpPath = new System.Windows.Forms.TextBox();
+            this.XMLPath = new System.Windows.Forms.TextBox();
+            this.radioYes = new System.Windows.Forms.RadioButton();
+            this.radioNo = new System.Windows.Forms.RadioButton();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axTE3DWindow1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTEInformationWindow1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(2, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(228, 319);
+            this.treeView1.TabIndex = 2;
+            // 
+            // axTE3DWindow1
+            // 
+            this.axTE3DWindow1.Enabled = true;
+            this.axTE3DWindow1.Location = new System.Drawing.Point(236, 27);
+            this.axTE3DWindow1.Name = "axTE3DWindow1";
+            this.axTE3DWindow1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTE3DWindow1.OcxState")));
+            this.axTE3DWindow1.Size = new System.Drawing.Size(874, 695);
+            this.axTE3DWindow1.TabIndex = 3;
+            // 
+            // axTEInformationWindow1
+            // 
+            this.axTEInformationWindow1.Enabled = true;
+            this.axTEInformationWindow1.Location = new System.Drawing.Point(2, 352);
+            this.axTEInformationWindow1.Name = "axTEInformationWindow1";
+            this.axTEInformationWindow1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTEInformationWindow1.OcxState")));
+            this.axTEInformationWindow1.Size = new System.Drawing.Size(228, 370);
+            this.axTEInformationWindow1.TabIndex = 4;
+            // 
+            // buttonGetTree
+            // 
+            this.buttonGetTree.Location = new System.Drawing.Point(6, 8);
+            this.buttonGetTree.Name = "buttonGetTree";
+            this.buttonGetTree.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetTree.TabIndex = 5;
+            this.buttonGetTree.Text = "获取树";
+            this.buttonGetTree.UseVisualStyleBackColor = true;
+            this.buttonGetTree.Click += new System.EventHandler(this.buttonGetTree_Click);
+            // 
+            // buttonSaveToXml
+            // 
+            this.buttonSaveToXml.Location = new System.Drawing.Point(101, 8);
+            this.buttonSaveToXml.Name = "buttonSaveToXml";
+            this.buttonSaveToXml.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveToXml.TabIndex = 6;
+            this.buttonSaveToXml.Text = "保存XML";
+            this.buttonSaveToXml.UseVisualStyleBackColor = true;
+            this.buttonSaveToXml.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1425, 25);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem1
+            // 
+            this.ToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem2,
+            this.ToolStripMenuItem3,
+            this.saveToolStripMenuItem});
+            this.ToolStripMenuItem1.Name = "ToolStripMenuItem1";
+            this.ToolStripMenuItem1.Size = new System.Drawing.Size(44, 21);
+            this.ToolStripMenuItem1.Text = "文件";
+            // 
+            // ToolStripMenuItem2
+            // 
+            this.ToolStripMenuItem2.Name = "ToolStripMenuItem2";
+            this.ToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItem2.Text = "打开";
+            this.ToolStripMenuItem2.Click += new System.EventHandler(this.ToolStripMenuItem2_Click);
+            // 
+            // ToolStripMenuItem3
+            // 
+            this.ToolStripMenuItem3.Name = "ToolStripMenuItem3";
+            this.ToolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItem3.Text = "关闭";
+            this.ToolStripMenuItem3.Click += new System.EventHandler(this.ToolStripMenuItem3_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "保存";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1112, 26);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(313, 696);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonGetTree);
+            this.tabPage1.Controls.Add(this.buttonSaveToXml);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(305, 670);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "XML获取";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.XMLPath);
+            this.tabPage2.Controls.Add(this.shpPath);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(305, 670);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "XML测试";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shp文件路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "XML文件路径：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(95, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "打开shp";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(95, 63);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "打开xml";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // shpPath
+            // 
+            this.shpPath.Location = new System.Drawing.Point(8, 37);
+            this.shpPath.Name = "shpPath";
+            this.shpPath.Size = new System.Drawing.Size(291, 21);
+            this.shpPath.TabIndex = 5;
+            // 
+            // XMLPath
+            // 
+            this.XMLPath.Location = new System.Drawing.Point(8, 91);
+            this.XMLPath.Name = "XMLPath";
+            this.XMLPath.Size = new System.Drawing.Size(291, 21);
+            this.XMLPath.TabIndex = 6;
+            // 
+            // radioYes
+            // 
+            this.radioYes.AutoSize = true;
+            this.radioYes.Checked = true;
+            this.radioYes.Location = new System.Drawing.Point(83, 31);
+            this.radioYes.Name = "radioYes";
+            this.radioYes.Size = new System.Drawing.Size(35, 16);
+            this.radioYes.TabIndex = 7;
+            this.radioYes.TabStop = true;
+            this.radioYes.Text = "是";
+            this.radioYes.UseVisualStyleBackColor = true;
+            // 
+            // radioNo
+            // 
+            this.radioNo.AutoSize = true;
+            this.radioNo.Location = new System.Drawing.Point(155, 31);
+            this.radioNo.Name = "radioNo";
+            this.radioNo.Size = new System.Drawing.Size(35, 16);
+            this.radioNo.TabIndex = 8;
+            this.radioNo.Text = "否";
+            this.radioNo.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(200, 629);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "加载";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioYes);
+            this.groupBox1.Controls.Add(this.radioNo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 64);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "是否显示注解：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1425, 725);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.axTEInformationWindow1);
+            this.Controls.Add(this.axTE3DWindow1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "skylinedemo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axTE3DWindow1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTEInformationWindow1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TreeView treeView1;
+        private AxTerraExplorerX.AxTE3DWindow axTE3DWindow1;
+        private AxTerraExplorerX.AxTEInformationWindow axTEInformationWindow1;
+        private System.Windows.Forms.Button buttonGetTree;
+        private System.Windows.Forms.Button buttonSaveToXml;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioNo;
+        private System.Windows.Forms.RadioButton radioYes;
+        private System.Windows.Forms.TextBox XMLPath;
+        private System.Windows.Forms.TextBox shpPath;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ skyline/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TerraExplorerX;
+using System.Xml.Linq;
+using System.IO;
+
+namespace skyline
+{
+    public partial class Form1 : Form
+    {
+        private SGWorld65 sgworld;
+        public Form1()
+        {
+            InitializeComponent();
+            sgworld = new SGWorld65();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void myScanTree()
+        {
+            try
+            {
+                var root = sgworld.ProjectTree.GetNextItem(string.Empty, ItemCode.ROOT);
+                string a = sgworld.ProjectTree.GetItemName(root);
+                string b = sgworld.ProjectTree.HiddenGroupName;
+                BuildTreeRecursive(null, root);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("错误信息：" + ex.Message);
+            }
+        }
+
+        private void BuildTreeRecursive(TreeNode node, String current)
+        {
+            while (String.IsNullOrEmpty(current)==false)
+            {
+                if (sgworld.ProjectTree.IsGroup(current)) {
+                    TreeNode groupnode = new TreeNode();
+                    string gname = sgworld.ProjectTree.GetItemName(current);
+                    groupnode.Text = gname;
+                    if (node != null)
+                    {
+                        node.Nodes.Add(groupnode);
+                    }
+                    else
+                    {
+                        treeView1.Nodes.Add(groupnode);
+                    }
+                    var child = sgworld.ProjectTree.GetNextItem(current, ItemCode.CHILD);
+                    BuildTreeRecursive(groupnode, child);
+                }
+                else
+                {
+                    var currentName = sgworld.ProjectTree.GetItemName(current);
+                    if (node == null)
+                    {
+                        treeView1.Nodes.Add(currentName);
+                    }
+                    else
+                    {
+                        node.Nodes.Add(currentName);
+                    }
+                }
+                current = sgworld.ProjectTree.GetNextItem(current, ItemCode.NEXT);
+            }
+
+        }
+
+        private void buttonGetTree_Click(object sender, EventArgs e)
+        {
+            myScanTree();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            if (node == null)
+            {
+                MessageBox.Show("请选择节点");
+                return;
+            }
+            String nodeName = node.Text;
+            IFeatureLayer65 layer = sgworld.ProjectTree.GetLayerByName(nodeName);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "XML files(*.xml)|*.xml";
+            saveFileDialog.DefaultExt = "xml";
+            DialogResult result = saveFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                XElement element = new XElement("Layer", new object[] { new XAttribute("Name", layer.TreeItem.Name), new XAttribute("IgnoreZ", layer.IgnoreZ), new XAttribute("Reproject", layer.Reproject), new XAttribute("Streaming", layer.Streaming) });
+                if (layer.FeatureGroups.Polygon != null)
+                {
+                    XElement content = layer.FeatureGroups.Polygon.WriteXmlSchema();
+                    content.Add(new XAttribute("Name", "Polygon"));
+                    element.Add(content);
+                }
+
+                if (layer.FeatureGroups.Annotation != null)
+                {
+                    XElement content = layer.FeatureGroups.Annotation.WriteXmlSchema();
+                    content.Add(new XAttribute("Name", "Annotation"));
+                    element.Add(content);
+                }
+                if (layer.FeatureGroups.Polyline != null)
+                {
+                    XElement content = layer.FeatureGroups.Polyline.WriteXmlSchema();
+                    content.Add(new XAttribute("Name", "Polyline"));
+                    element.Add(content);
+                }
+                if (layer.FeatureGroups.Point != null)
+                {
+                    XElement content = layer.FeatureGroups.Point.WriteXmlSchema();
+                    content.Add(new XAttribute("Name", "Point"));
+                    element.Add(content);
+                }
+
+                element.Save(saveFileDialog.FileName);
+            }
+
+        }
+
+        private void ToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "FLY文件(*.fly)|*.fly";
+            DialogResult result = open.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                sgworld.Project.Open(open.FileName, true); 
+            }
+        }
+
+        private void ToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            sgworld.Project.Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sgworld.Project.Save();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "SHP文件(*.shp)|*.shp";
+            DialogResult result = open.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                shpPath.Text = open.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "XML文件(*.xml)|*.xml";
+            DialogResult result = open.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                XMLPath.Text = open.FileName;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string spath = shpPath.Text;
+            string xpath = XMLPath.Text;
+            if (!File.Exists(spath))
+            {
+                MessageBox.Show("shp文件不存在", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!File.Exists(xpath))
+            {
+                MessageBox.Show("XML文件不存在", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string gid = sgworld.ProjectTree.FindOrCreateGroup("demo");
+
+            string tConnectionString = String.Format("FileName={0};TEPlugName=OGR;", spath);
+
+            IFeatureLayer65 featurelayer = sgworld.Creator.CreateFeatureLayer(Path.GetFileNameWithoutExtension(spath), tConnectionString, gid);
+            //featurelayer.Streaming = false;
+            featurelayer.DataSourceInfo.Attributes.ImportAll = true;
+            //featurelayer.Refresh();
+            string config = xpath;
+            
+            if (radioYes.Checked)
+            {
+                featurelayer.Annotation = true;
+            }
+            if (radioNo.Checked)
+            {
+                featurelayer.Annotation = false;
+            }
+            
+            featurelayer.ReadXmlSchema(config);
+            featurelayer.Refresh();
+
+ //           ISGWorldExtentions.sgWorld.Navigate.FlyToGuiZhou();
+        }
+
+
+    }
+}
